9b1d8b768b839feba73c6e5575e000fb8e6918b6
Vuk Isic <visicev@gmail.com>
2021-07-09 21:46:42 +0200

    [Blazor] App Added

---
+++ ToDoApp_Blazor/ToDoApp/Models/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+   public enum Status
+   {
+        TO_DO,
+        IN_PROGRESS,
+        DONE
+   }
+
+   public enum Priority
+   {
+        LOW,
+        MEDIUM,
+        HIGH
+   }
+}
---
+++ ToDoApp_Blazor/ToDoApp/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class Task
+    {
+        public long Id { get; set; }
+        public string TaskName { get; set; }
+        public Status Status { get; set; }
+        public Priority Priority { get; set; }
+
+        public Task(long id, string taskName, Status status, Priority priority)
+        {
+            Id = id;
+            TaskName = taskName;
+            Status = status;
+            Priority = priority;
+        }
+    }
+}
